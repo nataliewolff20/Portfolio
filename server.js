da13598f8e7f3a1eefcb28cada0426ba6b4a5eee
Natalie Wolff <natalielynnwolff@gmail.com>
2018-05-13 22:07:46 -0400

    basically done

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
+app.use(express.static('public'));
 
+app.get('/', function(req,res){
+  res.render('index.ejs');
+});
+
+app.get('/about', function(req,res){
+  res.render('about.ejs');
+});
 
 app.listen(port);
-console.log('----------------------');
-console.log('server running on port: ' + port);
-console.log('----------------------');
+console.log('---------------------------------');
+console.log('Server running on port: ' + port);
+console.log('---------------------------------');
